89a79e64810fafe937db368086187d9707511006
julialgalvao <julialgalvao777@gmail.com>
2023-04-14 11:05:36 -0300

    aula java

---
+++ script.js
@@ -0,0 +1,17 @@
+// switch
+
+let opcao =parseInt(prompt("Digite um numero"))
+
+//let opcao =2
+switch(opcao){
+    case 1:
+        document.write("primeira opcao")
+        break;
+    case 2:
+        document.write("Segunda opcao")
+        break;
+    case 3:
+        document.write("terceira opcao")
+    default:
+        document.write("opcao invalida")
+    }
